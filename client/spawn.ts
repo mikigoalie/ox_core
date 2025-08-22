090dd592be9f5a983c526ffa31d05cbe94c6f23f
mikigoalie <mikigoalie@seznam.cz>
2025-08-22 13:29:27 +0200

    Update spawn.ts

--- client/spawn.ts
+++ client/spawn.ts
@@ -63,7 +63,8 @@ netEvent('ox:startCharacterSelect', async (_userId: number, characters: Characte
 
   RequestCollisionAtCoord(x, y, z);
   FreezeEntityPosition(cache.ped, true);
-  SetEntityCoordsNoOffset(cache.ped, x, y, z, true, true, false);
+  const [success, groundZ] = GetGroundZAndNormalFor_3dCoord(x, y, z)
+  SetEntityCoordsNoOffset(cache.ped, x, y, (success && (z - groundZ) > 6.0) ? groundZ + 1 : z, true, true, false)
   SetEntityHeading(cache.ped, heading);
 
   SwitchOutPlayer(cache.ped, 1 | 8192, 1);
